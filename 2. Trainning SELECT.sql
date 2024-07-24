43d01f88815c685afffb2db038e1635935e1683b
Guilherme Lima Pereira <161603464+guilimap@users.noreply.github.com>
2024-07-24 15:59:40 -0300

    Add files via upload

---
+++ 2. Trainning SELECT.sql
@@ -0,0 +1,11 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+SELECT  
+first_name,
+last_name, 
+birth_date,
+age + 10
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
