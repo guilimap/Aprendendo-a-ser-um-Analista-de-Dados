468e9e62afaef5a498ab01e3f1211682d590ab4e
Guilherme Lima Pereira <161603464+guilimap@users.noreply.github.com>
2024-07-30 13:32:19 -0300

    Add files via upload

---
+++ 10. Trainning SUBQUERIES.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM employee_demographics
+WHERE employee_id IN 
+				( SELECT employee_id
+					FROM employee_salary
+                    WHERE dept_id = 1
+
+
+) ;
+
+SELECT first_name, salary, (SELECT AVG(salary)
+FROM employee_salary) AS AVG
+FROM employee_salary
+GROUP BY first_name, salary
+;
+
